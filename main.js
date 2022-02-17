277ad256e7813558f7ba728f4b6ca761b4a7ae69
Juan Pablo <juanpablo.ferro@desoft.cu>
2022-02-17 11:24:30 +0100

    adding listeners for the input event of every element and implementing some initial logic

--- main.js
+++ main.js
@@ -3,3 +3,29 @@ const $ = selector => document.querySelector(selector)
 const $js = $("js")
 const $css = $("css")
 const $html = $("html")
+
+const update = () => {
+  const html = createHtml()
+  $('iframe').setAttribute('srcdoc', html)
+}
+const createHtml = () => {
+  const html = $html.value
+  const css = $css.value
+  const js = $js.value
+
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <style>${css}</style>
+      </head>
+      <body>
+        ${html}
+        <script> ${js} </script>
+      </body>
+    </html>`
+}
+
+$js.addEventListener('input', update)
+$css.addEventListener('input', update)
+$html.addEventListener('input', update)
