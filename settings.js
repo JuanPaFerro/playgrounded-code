cc4afbfee690604de62b8c253a149ab236d168c8
unknown <juanpabloferroarroyo@gmail.com>
2022-02-20 20:08:42 +0100

    updating the settings view whith the data from the state

--- settings.js
+++ settings.js
@@ -1,12 +1,27 @@
 import { $$ } from "./utilities/dom-managment";
 import { getState } from "./state";
 
-const { updateSettings } = getState();
+const settings = $$("#settings [data-for]");
+const { updateSettings, ...restOfSettings } = getState();
 
-$$("#settings [data-for]").forEach((el) => {
+settings.forEach((el) => {
+  const settingKey = el.getAttribute("data-for");
+  const actualSettingValue = restOfSettings[settingKey];
+  const isSelect = el.nodeName === "SELECT";
+  const isCheckbox = el.nodeName === "INPUT" && el.type === "checkbox";
+  if (isSelect) {
+    const optionToSelect = el.querySelector(
+      `option[value="${actualSettingValue}"]`
+    );
+    optionToSelect && optionToSelect.setAttribute("selected", "");
+  } else if (isCheckbox) {
+    el.checked = actualSettingValue;
+  } else {
+    el.value = actualSettingValue;
+  }
+
   el.addEventListener("change", ({ target }) => {
     const { checked, value } = target;
-    const settingKey = target.getAttribute("data-for");
     const isNumber = target.getAttribute("type") === "number";
 
     let settingValue = typeof checked === "boolean" ? checked : value;
